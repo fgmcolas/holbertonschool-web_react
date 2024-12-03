dc6f2f8c960de566f11ba5c90198b6a745d42473
fgmcolas <https://github.com/fgmcolas>
2024-12-03 03:16:20 +0100

    Task 7

--- task_2/js/main.ts
+++ task_2/js/main.ts
@@ -54,6 +54,6 @@ function createEmployee(salary: number | string): Director | Teacher {
     }
 }
 
-console.log(createEmployee(200));
-console.log(createEmployee(1000));
-console.log(createEmployee('$500'));
+console.log(createEmployee(200).toString());
+console.log(createEmployee(1000).toString());
+console.log(createEmployee('$500').toString());
