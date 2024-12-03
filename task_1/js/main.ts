1c5725f62e440cbe8627d21aa07542e77a453032
fgmcolas <https://github.com/fgmcolas>
2024-12-03 02:31:52 +0100

    Task 3

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -7,12 +7,25 @@ interface Teacher {
     [propName: string]: any;
 }
 
-const teacher3: Teacher = {
-    firstName: 'Chaussure',
-    fullTimeEmployee: false,
-    lastName: 'Fromage',
-    location: 'Oui',
-    contract: false,
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'BBBWWWWAAAHH',
+    lastName: 'OHHHHHHH',
+    location: 'HAHAHA',
+    fullTimeEmployee: true,
+    numberOfReports: 99999,
+};
+console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName[0]}. ${lastName}`;
 };
 
-console.log(teacher3);
+console.log(printTeacher("Fromage", "Chaussure"));
