11c6675463834a41b57d4f74a2318d0aa57e4507
fgmcolas <https://github.com/fgmcolas>
2024-12-03 02:45:00 +0100

    Task 4

--- task_1/js/main.ts
+++ task_1/js/main.ts
@@ -11,15 +11,6 @@ interface Directors extends Teacher {
     numberOfReports: number;
 }
 
-const director1: Directors = {
-    firstName: 'BBBWWWWAAAHH',
-    lastName: 'OHHHHHHH',
-    location: 'HAHAHA',
-    fullTimeEmployee: true,
-    numberOfReports: 99999,
-};
-console.log(director1);
-
 interface printTeacherFunction {
     (firstName: string, lastName: string): string;
 }
@@ -28,4 +19,35 @@ const printTeacher: printTeacherFunction = (firstName, lastName) => {
     return `${firstName[0]}. ${lastName}`;
 };
 
-console.log(printTeacher("Fromage", "Chaussure"));
+interface StudentConstructor {
+    new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+    firstName: string;
+    lastName: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements StudentInterface {
+    firstName: string;
+    lastName: string;
+
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
+
+const student: StudentInterface = new StudentClass("Holby", "Choupi");
+console.log(student.displayName());
+console.log(student.workOnHomework());
