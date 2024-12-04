1291bb3aa880a30af58d53db1130ff3c9abcdf27
fgmcolas <https://github.com/fgmcolas>
2024-12-04 20:05:30 +0100

    task 2

--- Webpack/task_2/js/dashboard_main.js
+++ Webpack/task_2/js/dashboard_main.js
@@ -6,7 +6,6 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<p>Click here to get started</p>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
-$('body').prepend('<div id="logo"></div>')
 
 let count = 0;
 
