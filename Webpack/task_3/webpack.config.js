c5dc5545b92789dbc94f7a74dff21af480653d66
fgmcolas <https://github.com/fgmcolas>
2024-12-06 15:50:51 +0100

    Switching static property

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -22,6 +22,9 @@ module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public'),
+        },
         contentBase: path.resolve(__dirname, 'public'),
         port: 8564,
         open: true,
