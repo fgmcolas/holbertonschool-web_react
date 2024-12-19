88028dbc353bda8a26f50a431ea1861cfd5178fb
fgmcolas <https://github.com/fgmcolas>
2024-12-19 20:52:49 +0100

    Better indentation

--- react_props/task_1/dashboard/src/Login/Login.spec.js
+++ react_props/task_1/dashboard/src/Login/Login.spec.js
@@ -4,12 +4,10 @@ import Login from './Login';
 
 test('testing signin form elements', () => {
     render(<Login />);
-
     const inputElements = screen.getAllByRole('textbox')
     const emailLabelElement = screen.getByLabelText(/email/i);
     const passwordLabelElement = screen.getByLabelText(/password/i);
     const buttonElementText = screen.getByRole('button', { name: 'OK' })
-
     expect(inputElements).toHaveLength(2)
     expect(emailLabelElement).toBeInTheDocument()
     expect(passwordLabelElement).toBeInTheDocument()
@@ -18,26 +16,18 @@ test('testing signin form elements', () => {
 
 test('it should check that the email input element will be focused whenever the associated label is clicked', async () => {
     render(<Login />)
-
     const emailLabel = screen.getByText('Email');
     const emailInput = screen.getByLabelText('Email', { selector: 'input' });
-
     fireEvent.click(emailLabel);
-
     emailInput.focus();
-
     expect(emailInput).toHaveFocus();
 })
 
 test('it should check that the password input element will be focused whenver the associated label is clicked', async () => {
     render(<Login />)
-
     const passwordLabel = screen.getByText('Password');
     const passwordInput = screen.getByLabelText('Password', { selector: 'input' });
-
     fireEvent.click(passwordLabel);
-
     passwordInput.focus()
-
     expect(passwordInput).toHaveFocus();
 });
