d8babed64c344a91b9dbb474f572a9d110d6a0b5
fgmcolas <https://github.com/fgmcolas>
2024-12-19 22:58:43 +0100

    Task 4

--- react_props/task_4/dashboard/src/App/App.jsx
+++ react_props/task_4/dashboard/src/App/App.jsx
@@ -2,7 +2,13 @@ import Notifications from "../Notifications/Notifications.jsx";
 import Header from "../Header/Header.jsx";
 import Login from "../Login/Login.jsx";
 import Footer from "../Footer/Footer.jsx";
+import CourseList from "../CourseList/CourseList.jsx";
+import PropTypes from 'prop-types';
 import { getLatestNotification } from "../utils/utils";
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
 
 const notificationsList = [
   { id: 1, type: 'default', value: 'New course available' },
@@ -10,15 +16,30 @@ const notificationsList = [
   { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
 ];
 
-export default function App() {
+const coursesList = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 }
+];
+
+export default function App({ isLoggedIn = false }) {
 
   return (
     <>
-      <Notifications notifications = {notificationsList} />
-      <Header />
-      <Login />
+      <Notifications
+        notifications = {notificationsList}
+      />
+      <>
+        <Header />
+        {
+          !isLoggedIn ? (
+            <Login />
+          ) : (
+            <CourseList courses = {coursesList} />
+          )
+        }
+      </>
       <Footer />
     </>
-  )
+  );
 }
-
