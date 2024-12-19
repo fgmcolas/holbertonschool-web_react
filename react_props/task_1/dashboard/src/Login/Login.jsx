1f2b549fe8d83e554e87cb6fad44f93e68c7f5dc
fgmcolas <https://github.com/fgmcolas>
2024-12-19 20:52:02 +0100

    .spec.js files upgraded

--- react_props/task_1/dashboard/src/Login/Login.jsx
+++ react_props/task_1/dashboard/src/Login/Login.jsx
@@ -5,9 +5,9 @@ function Login() {
         <div className="App-login">
             <p>Login to access the full dashboard</p>
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
-            <label htmlFor="pass">Password</label>
-            <input type="password" name="pass" id="pass" />
+            <input type="email" name="user_email" id="email" />
+            <label htmlFor="password">Password</label>
+            <input type="text" name="user_password" id="password" />
             <button role="button" type="submit">OK</button>
         </div>
     );
