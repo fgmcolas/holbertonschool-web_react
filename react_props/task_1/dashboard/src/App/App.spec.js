bb895bfdedcdcd4cdbc890d8e201390b76ec545f
fgmcolas <https://github.com/fgmcolas>
2024-12-19 20:13:06 +0100

    .spec.js files upgraded

--- react_props/task_1/dashboard/src/App/App.spec.js
+++ react_props/task_1/dashboard/src/App/App.spec.js
@@ -6,5 +6,5 @@ test("Should render the header, login, and footer components", () => {
   render(<App />);
   expect(screen.getByText(/School dashboard/i)).toBeInTheDocument();
   expect(screen.getByText(/Login to access the full dashboard/i)).toBeInTheDocument();
-  expect(screen.getByText(/Copyright 2024 Holberton School/i)).toBeInTheDocument();
+  expect(screen.getByText(/Copyright 2024 - Holberton School/i)).toBeInTheDocument();
 });
--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { expect, test } from "@jest/globals";
 import Footer from "./Footer";
+import { getCurrentYear, getFooterCopy } from "../utils/utils";
 
 test("Should render footer with correct copyright text", () => {
+    const expectedFooterText = 'Copyright ${getCurrentYear()} - ${getFooterCopy(false)}';
     render(<Footer />);
-    const footerText = screen.getByText('Copyright {getCurrentYear} - Holberton School');
+    const footerText = screen.getByText(expectedFooterText);
     expect(footerText).toBeInTheDocument();
 });
