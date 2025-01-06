fc9893eb0bbbe64b077a158e7db706342fc5b220
fgmcolas <https://github.com/fgmcolas>
2025-01-06 10:17:12 +0100

    Task 1

--- react_component/task_1/dashboard/src/App/App.spec.js
+++ react_component/task_1/dashboard/src/App/App.spec.js
@@ -17,7 +17,6 @@ test('Should return true if the App component is a class component', () => {
   const props = Object.getOwnPropertyNames(App.prototype);
   const isClassComponent = App.prototype.__proto__ === React.Component.prototype;
   const inheritsFromReactComponent = Object.getPrototypeOf(App.prototype) === React.Component.prototype;
-
   expect(props).toContain('constructor');
   expect(isClassComponent).toBe(true);
   expect(inheritsFromReactComponent).toBe(true);
@@ -25,38 +24,28 @@ test('Should return true if the App component is a class component', () => {
 
 test('Should call the logOut prop once whenever the user hits "Ctrl" + "h" keyboard keys', () => {
   const logOutMock = jest.fn();
-
   jest.spyOn(window, 'alert').mockImplementation(() => { });
   render(<App isLoggedIn={true} logOut={logOutMock} />);
-
   fireEvent.keyDown(document, { ctrlKey: true, key: 'h' });
-
   expect(logOutMock).toHaveBeenCalledTimes(1);
 });
 
 test('Should display an alert window whenever the user hit "ctrl" + "h" keyboard keys', () => {
   const logoutSpy = jest.fn();
   window.alert = jest.fn();
-
   render(<App logOut={logoutSpy} />);
-
   fireEvent.keyDown(document, { ctrlKey: true, key: 'h' });
-
   expect(window.alert).toHaveBeenCalledWith('Logging you out');
 });
 
 test('Should remove event listener in componentWillUnmount', () => {
   const { unmount } = render(<App isLoggedIn={false} />);
-
   expect(document.addEventListener).toHaveBeenCalledWith('keydown', expect.any(Function));
-
   unmount();
-
   expect(document.removeEventListener).toHaveBeenCalledWith('keydown', expect.any(Function));
 });
 
 test('should add event listener in componentDidMount', () => {
   render(<App isLoggedIn={false} />);
-
   expect(document.addEventListener).toHaveBeenCalledWith('keydown', expect.any(Function));
 });
