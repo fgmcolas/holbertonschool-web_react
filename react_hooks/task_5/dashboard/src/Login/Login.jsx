ceeb1f9ba27b5fb5e064bebc68f3bcf15092d2ad
fgmcolas <https://github.com/fgmcolas>
2025-01-21 18:22:51 +0100

    Task 7

--- react_hooks/task_5/dashboard/src/Login/Login.jsx
+++ react_hooks/task_5/dashboard/src/Login/Login.jsx
@@ -1,80 +1,47 @@
 import React from 'react';
 import WithLogging from '../HOC/WithLogging';
+import useLogin from '../hooks/useLogin';
 import './Login.css';
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            email: props.email || '',
-            password: props.password || '',
-            enableSubmit: false,
-        };
-    }
+const Login = ({ login }) => {
+    const {
+        email,
+        password,
+        enableSubmit,
+        handleChangeEmail,
+        handleChangePassword,
+        handleLoginSubmit,
+    } = useLogin({
+        onLogin: login
+    });;
 
-    handleLoginSubmit = (e) => {
-        e.preventDefault();
-        const { email, password } = this.state;
-        this.props.logIn(email, password)
-    }
-
-    validateEmail = (email) => {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return emailRegex.test(email);
-    };
-
-    handleChangeEmail = (e) => {
-        const email = e.target.value;
-        const { password } = this.state;
-        this.setState({
-            email: email,
-            enableSubmit: this.validateEmail(email) && password.length >= 8,
-        });
-    };
-
-    handleChangePassword = (e) => {
-        const password = e.target.value;
-        const { email } = this.state;
-        this.setState({
-            password: password,
-            enableSubmit: this.validateEmail(email) && password.length >= 8,
-        });
-    };
-
-    render() {
-        const { enableSubmit, email, password } = this.state;
-        return (
-            <form aria-label="form" onSubmit={this.handleLoginSubmit}>
-                <div className="App-body">
-                    <p>Login to access the full dashboard</p>
-                    <div className="form">
-                        <label htmlFor="email">Email: </label>
-                        <input
-                            type="email"
-                            name="user_email"
-                            id="email"
-                            value={email}
-                            onChange={this.handleChangeEmail}
-                        />
-                        <label htmlFor="password">Password: </label>
-                        <input
-                            type="text"
-                            name="user_password"
-                            id="password"
-                            value={password}
-                            onChange={this.handleChangePassword}
-                        />
-                        <input
-                            value="OK"
-                            type="submit"
-                            disabled={!enableSubmit}
-                        />
-                    </div>
+    return (
+        <form aria-label="form" onSubmit={handleLoginSubmit}>
+            <div className="App-body">
+                <p>Login to access the full dashboard</p>
+                <div className="form">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        name="user_email"
+                        id="email"
+                        value={email}
+                        onChange={handleChangeEmail}
+                    />
+                    <label htmlFor="password">Password</label>
+                    <input
+                        type="password"
+                        name="user_password"
+                        id="password"
+                        value={password}
+                        onChange={handleChangePassword}
+                    />
+                    <input value="OK" type="submit" disabled={!enableSubmit} />
                 </div>
-            </form>
-        )
-    }
-}
+            </div>
+        </form>
+    );
+};
 
-const LoginWithLogging = WithLogging(Login)
+const LoginWithLogging = WithLogging(Login);
 export default LoginWithLogging;
