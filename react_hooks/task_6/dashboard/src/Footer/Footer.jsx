3d75a5c4c69088180c1ad4dd9a72f0a3ba037671
fgmcolas <https://github.com/fgmcolas>
2025-01-21 18:25:58 +0100

    Task 8

--- react_hooks/task_6/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_6/dashboard/src/Footer/Footer.jsx
@@ -1,14 +1,11 @@
-import { useContext } from 'react';
 import './Footer.css';
 import { getCurrentYear, getFooterCopy } from '../utils/utils';
-import { newContext } from '../Context/context';
 
-export default function Footer() {
-    const { user } = useContext(newContext);
+export default function Footer({ user }) {
     return (
         <div className="App-footer">
             <p>Copyright {getCurrentYear()} - {getFooterCopy(true)}</p>
             {user.isLoggedIn && <a href="#">Contact us</a>}
         </div>
     );
-};
+}
--- react_hooks/task_6/dashboard/src/Header/Header.jsx
+++ react_hooks/task_6/dashboard/src/Header/Header.jsx
@@ -1,18 +1,15 @@
-import { useContext } from 'react';
 import './Header.css';
 import HolbertonLogo from '../assets/holberton-logo.jpg';
-import { newContext } from '../Context/context';
 
-const Header = () => {
-    const { user, logOut } = useContext(newContext);
+const Header = ({ user, logOut }) => {
     return (
         <div className="App-header">
             <img className="App-logo" src={HolbertonLogo} alt="holberton logo" />
             <h1>School dashboard</h1>
             {user.isLoggedIn && (
-                <section id="logoutSection">
-                    Welcome {user.email} (<a href="" onClick={logOut}>logout</a>)
-                </section>
+                <div id="logoutSection">
+                    Welcome <b>{user.email}</b> (<a href="" onClick={logOut}>logout</a>)
+                </div>
             )}
         </div>
     );
